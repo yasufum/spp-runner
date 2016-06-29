49357196a9bd0ef21cdad5765223e47742b6cc23
Yasufumi Ogawa <ogawa.yasufumi@lab.ntt.co.jp>
2016-06-30 02:35:15 +0900

    update

--- runscripts/secondary.sh
+++ runscripts/secondary.sh
@@ -12,4 +12,4 @@ echo $SPPDIR/nfv/nfv/x86_64-ivshmem-linuxapp-gcc/app/spp_nfv \
 $SPPDIR/nfv/nfv/x86_64-ivshmem-linuxapp-gcc/app/spp_nfv \
   -c ${COREMASK} -n 4 --proc-type=secondary \
   -- -n ${CLIENT_ID} -s 192.168.122.1:${SEC_PORT}
-#  2>&1 > log/secondary-${CLIENT_ID}.log
+  2>&1 > log/secondary-${CLIENT_ID}.log

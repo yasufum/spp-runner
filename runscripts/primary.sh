bfa09e0ca86980fb2e4fa6ee15f28a8c7a3af3d6
yogawa <geminoa@gmail.com>
2017-01-17 21:32:42 +0900

    refactor

--- runscripts/primary.sh
+++ runscripts/primary.sh
@@ -1,15 +1,17 @@
 #!/bin/sh 
 
 SPPDIR=$1
-CTR_HOST=$2
-CTR_PRI_PORT=$3
+COREMASK=$2
+CTR_HOST=$3
+CTR_PRI_PORT=$4
+PORTMASK=0x03
 NUM_RINGS=10
 
-sudo -E $SPPDIR/primary/src/primary/x86_64-ivshmem-linuxapp-gcc/spp_primary \
-  -c 0x02 -n 4 --socket-mem 1024 \
+sudo -E ${SPPDIR}/primary/src/primary/x86_64-ivshmem-linuxapp-gcc/spp_primary \
+  -c ${COREMASK} -n 4 --socket-mem 1024 \
   --huge-dir=/dev/hugepages \
   --proc-type=primary \
   -- \
-  -p 0x03 -n ${NUM_RINGS} \
+  -p ${PORTMASK} -n ${NUM_RINGS} \
   -s ${CTR_HOST}:${CTR_PRI_PORT} #\
 #  2>&1 > log/primary.log
--- wakeup.py
+++ wakeup.py
@@ -1,105 +1,155 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 # Run SPP, then QEMU within tmux.
 
 import os
-
+import sys
 
 # params
+sess_name = "spp"
 run_dir = "."
 spp_srcdir = "$HOME/dpdk-home/spp/src"
 qemu_dir = "$HOME/dpdk-home/qemu-setup/runscripts"
 
-ctrl_host = "127.0.0.1"
-ctrl_pri_port = 5555
-ctrl_sec_port = 6666
+# spp controller
+ctrler = {
+        "host": "127.0.0.1",
+        "pri_port": 5555,
+        "sec_port": 6666
+        }
 
+# spp primary
+primary = {"coremask": "0x03"}
+
+# spp secondaries
 secondaries = [
-        {"id": "1", "coremask": "0x06"},
-        {"id": "2", "coremask": "0x0A"},
+        {"id": 1, "coremask": "0x0C"},
+        {"id": 2, "coremask": "0x30"},
+        {"id": 3, "coremask": "0xC0"},
         ]
 
-monitor_port = "4444"
+# ring VM
+ring_sh = "ring.sh"
+ring_vms = [
+        {"id": 1}
+        ]
 
-sess_name = "spp"
+# vhost VM
+vhost_sh = "vhost.sh"
+vhost_vms = [
+        {"id": 1}
+        ]
 
+# return tmux windows
+def setup_windows(nof_sec, nof_ring, nof_vhost):
+    windows = [
+            {
+                "win_name": "ctrler",
+                "dir": spp_srcdir,
+                "cmd": "python spp.py",
+                "opts": "-p %s -s %s" % (
+                    ctrler.get("pri_port"), ctrler.get("sec_port")),
+                "enter_key": True
+                },
 
-# tmux windows
-windows = [
-   {
-    "win_name": "ctrler",
-    "dir": spp_srcdir,
-    "cmd": "python spp.py",
-    "opts": "-p %s -s %s" % (ctrl_pri_port, ctrl_sec_port),
-    "enter": True
-    },
+            {
+                "win_name": "pri",
+                "dir": run_dir,
+                "cmd": "sh runscripts/primary.sh",
+                "opts": "%s %s %s %s" % (
+                    spp_srcdir, primary.get("coremask"),
+                    ctrler.get("host"),
+                    ctrler.get("pri_port")),
+                "enter_key": True
+                }
+            ]
 
-   {
-    "win_name": "pri",
-    "dir": run_dir,
-    "cmd": "sh runscripts/primary.sh",
-    "opts": "%s %s %s" % (spp_srcdir, ctrl_host, ctrl_pri_port),
-    "enter": True
-    },
+    for i in range(nof_sec):
+        windows.append({
+            "win_name": "sec%s" % i,
+            "dir": run_dir,
+            "cmd": "sh runscripts/secondary.sh",
+            "opts": "%s %s %s %s %s" % (
+                spp_srcdir,
+                secondaries[i].get("id"),
+                secondaries[i].get("coremask"),
+                ctrler.get("host"),
+                ctrler.get("sec_port")),
+            "enter_key": True
+            })
 
-   {
-    "win_name": "sec1",
-    "dir": run_dir,
-    "cmd": "sh runscripts/secondary.sh",
-    "opts": "%s %s %s %s %s" % (spp_srcdir, secondaries[0].get("id"), secondaries[0].get("coremask"), ctrl_host, ctrl_sec_port),
-    "enter": True
-    },
+    for i in range(nof_ring):
+        windows.append({
+            "win_name": "vm_r%s" % i,
+            "dir": qemu_dir,
+            "cmd": "./%s" % ring_sh,
+            "opts": "%s" % ring_vms[i].get("id"),
+            "enter_key": True
+            })
 
-   {
-    "win_name": "sec2",
-    "dir": run_dir,
-    "cmd": "sh runscripts/secondary.sh",
-    "opts": "%s %s %s %s %s" % (spp_srcdir, secondaries[1].get("id"), secondaries[1].get("coremask"), ctrl_host, ctrl_sec_port),
-    "enter": True
-    },
-
-   {
-    "win_name": "qemu",
-    "dir": qemu_dir,
-    "cmd": "",
-    "opts": "",
-    "enter": False 
-    },
-
-   {
-    "win_name": "monitor",
-    "dir": qemu_dir,
-    "cmd": "telnet",
-    "opts": "localhost %s" %  monitor_port,
-    "enter": False
-    }
-
-  ]
+    for i in range(nof_vhost):
+        windows.append({
+            "win_name": "vm_v%s" % i,
+            "dir": qemu_dir,
+            "cmd": "./%s" % vhost_sh,
+            "opts": "%s" % vhost_vms[i].get("id"),
+            "enter_key": True
+            })
+            
+    return windows
 
 
 def main():
-  cmd = []  # contains tmux commands
-  for w in windows:
-    if len(cmd) == 0:
-      new_sess = "tmux new-session -d -s %s -n %s -c %s" % (sess_name, w.get("win_name"), w.get("dir"))
-      send_keys = "tmux send-keys -t %s \"%s %s\" C-m" % (w.get("win_name"), w.get("cmd"), w.get("opts"))
-      cmd.append(new_sess)
-      cmd.append(send_keys)
+    # [TODO] 引数の取り方を見直す
+    args = sys.argv
+    if len(args) > 1:
+        nof_sec = int(args[1])
     else:
-      new_win = "tmux new-window -n %s -c \"%s\"" % (w.get("win_name"), w.get("dir"))
-      if w.get("enter") == True:
-        send_keys = "tmux send-keys -t %s \"%s %s\" C-m" % (w.get("win_name"), w.get("cmd"), w.get("opts"))
-      else:
-        send_keys = "tmux send-keys -t %s \"%s %s\"" % (w.get("win_name"), w.get("cmd"), w.get("opts"))
-      cmd.append(new_win)
-      cmd.append(send_keys)
+        nof_sec = len(secondaries)
 
-  cmd.append("tmux attach -t %s" % sess_name)
- 
+    if nof_sec > len(secondaries):
+        nof_sec = len(secondaries)
 
-  for c in cmd:
-    print(c)
-    os.system(c)
+    nof_ring = len(ring_vms)
+    nof_vhost = len(vhost_vms)
+
+    cmd = []  # contains tmux commands
+    for w in setup_windows(nof_sec, nof_ring, nof_vhost):
+        if len(cmd) == 0:
+            new_sess = "tmux new-session -d -s %s -n %s -c %s" % (
+                    sess_name,
+                    w.get("win_name"),
+                    w.get("dir"))
+            send_keys = "tmux send-keys -t %s \"%s %s\" C-m" % (
+                    w.get("win_name"),
+                    w.get("cmd"),
+                    w.get("opts"))
+            cmd.append(new_sess)
+            cmd.append(send_keys)
+        else:
+            new_win = "tmux new-window -n %s -c \"%s\"" % (
+                    w.get("win_name"),
+                    w.get("dir"))
+            if w.get("enter_key") == True:
+                send_keys = "tmux send-keys -t %s \"%s %s\" C-m" % (
+                      w.get("win_name"),
+                      w.get("cmd"),
+                      w.get("opts"))
+            else:
+                send_keys = "tmux send-keys -t %s \"%s %s\"" % (
+                      w.get("win_name"),
+                      w.get("cmd"),
+                      w.get("opts"))
+            cmd.append(new_win)
+            cmd.append(send_keys)
+  
+    cmd.append("tmux attach -t %s" % sess_name)
+   
+  
+    for c in cmd:
+        print(c)
+        os.system(c)
 
 if __name__ == '__main__':
-  main()
+    main()

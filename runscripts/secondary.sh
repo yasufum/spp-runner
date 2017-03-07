675042bbb1424baf8fc392d27c0ac510def89040
yogawa <geminoa@gmail.com>
2017-03-07 21:40:27 +0900

    refactor

--- runscripts/secondary.sh
+++ runscripts/secondary.sh
@@ -6,13 +6,15 @@ COREMASK=$3
 CTR_HOST=$4
 CTR_SEC_PORT=$5
 
+NUM_MEM_CHAN=4
+
 LOGFILE=log/secondary-${CLIENT_ID}.log
 
 echo "Runnig spp sec"${CLIENT_ID}" ..."
 echo "log file: "${LOGFILE}
 
-sudo -E $SPPDIR/nfv/src/nfv/x86_64-ivshmem-linuxapp-gcc/app/spp_nfv \
-  -c ${COREMASK} -n 4 --proc-type=secondary \
+sudo -E ${SPPDIR}/nfv/src/nfv/x86_64-ivshmem-linuxapp-gcc/app/spp_nfv \
+  -c ${COREMASK} -n ${NUM_MEM_CHAN} --proc-type=secondary \
   -- \
   -n ${CLIENT_ID} -s ${CTR_HOST}:${CTR_SEC_PORT} \
   2>&1 > ${LOGFILE} \

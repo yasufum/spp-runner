81bba2cb18171fa5d6646915f5617d5811deb57d
Yasufumi Ogawa <ogawa.yasufumi@lab.ntt.co.jp>
2017-10-18 13:12:21 +0900

    runscripts: refactor to meet pep8
    Signed-off-by: Yasufumi Ogawa <ogawa.yasufumi@lab.ntt.co.jp>

--- runscripts/primary.py
+++ runscripts/primary.py
@@ -1,65 +1,58 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import os
 import argparse
+import os
 import subprocess
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Launch primary process")
-    
+
     parser.add_argument(
-            "-d", "--sppdir", 
-            type=str, default='.',
-            help="SPP dir"
-            )
+        "-d", "--sppdir",
+        type=str, default='.',
+        help="SPP dir")
     parser.add_argument(
-            "-c", "--coremask", 
-            type=str, default='0x03',
-            help="coremask"
-            )
+        "-c", "--coremask",
+        type=str, default='0x03',
+        help="coremask")
     parser.add_argument(
-            "-p", "--portmask", 
-            type=str, default='0x0f',
-            help="portmask"
-            )
+        "-p", "--portmask",
+        type=str, default='0x0f',
+        help="portmask")
     parser.add_argument(
-            "-ch", "--ctrl-host", 
-            type=str, default='127.0.0.1',
-            help="controller ipaddr"
-            )
+        "-ch", "--ctrl-host",
+        type=str, default='127.0.0.1',
+        help="controller ipaddr")
     parser.add_argument(
-            "-cp", "--ctrl-port", 
-            type=int, default=5555,
-            help="controller port num"
-            )
+        "-cp", "--ctrl-port",
+        type=int, default=5555,
+        help="controller port num")
     parser.add_argument(
-            "-n", "--num-ring", 
-            type=int, default=10,
-            help="The number of rings"
-            )
+        "-n", "--num-ring",
+        type=int, default=10,
+        help="The number of rings")
     parser.add_argument(
-            "-m", "--mem", 
-            type=int, default=1024,
-            help="memory size"
-            )
+        "-m", "--mem",
+        type=int, default=1024,
+        help="memory size")
     parser.add_argument(
-            "-mc", "--mem-chan", 
-            type=int, default=4,
-            help="The number of memory channels"
-            )
+        "-mc", "--mem-chan",
+        type=int, default=4,
+        help="The number of memory channels")
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
     cmd_path = '%s/primary/%s/spp_primary' % (
-            args.sppdir, os.getenv('RTE_TARGET')
-            )
+        args.sppdir, os.getenv('RTE_TARGET'))
+
     if not os.path.isfile(cmd_path):
         cmd_path = '%s/primary/src/primary/%s/spp_primary' % (
-                args.sppdir, os.getenv('RTE_TARGET')
-                )
+            args.sppdir, os.getenv('RTE_TARGET'))
+
     subprocess.call('sudo pwd', shell=True)
     cmd = 'sudo -E %s \\\n' % cmd_path
     cmd += '  -c %s \\\n' % args.coremask
@@ -71,7 +64,7 @@ def main():
     cmd += '  -p %s \\\n' % args.portmask
     cmd += '  -n %d \\\n' % args.num_ring
     cmd += '  -s %s:%d \\\n' % (args.ctrl_host, args.ctrl_port)
-    
+
     print(cmd)
     subprocess.call(cmd, shell=True)
 
--- runscripts/secondaries.py
+++ runscripts/secondaries.py
@@ -3,11 +3,10 @@
 
 # Run multiple spp secondaries in a single term
 
-import os
-import sys
-import yaml
 import argparse
+import os
 import subprocess
+import yaml
 
 # This command is intended to run from parent dir,
 # so config file must be in the dir.
@@ -15,33 +14,35 @@ conf_path = "conf.yml"
 
 home_dir = os.environ["HOME"]
 
+
 def parse_args():
     parser = argparse.ArgumentParser(description="Start up spp secondaries")
     parser.add_argument(
-            "-i", "--id",
-            type=int,
-            help="Secondary id")
+        "-i", "--id",
+        type=int,
+        help="Secondary id")
     parser.add_argument(
-            "-n", "--num",
-            type=int, default=2,
-            help="Number of SPP secondaries")
+        "-n", "--num",
+        type=int, default=2,
+        help="Number of SPP secondaries")
     parser.add_argument(
-            "-d", "--sppdir",
-            type=str, default="%s/dpdk-home/spp/src" % home_dir,
-            help="SPP's working dir")
+        "-d", "--sppdir",
+        type=str, default="%s/dpdk-home/spp/src" % home_dir,
+        help="SPP's working dir")
     parser.add_argument(
-            "-nm", "--num-memchan",
-            type=int, default=4,
-            help="Number of memory channels")
+        "-nm", "--num-memchan",
+        type=int, default=4,
+        help="Number of memory channels")
     parser.add_argument(
-            "-ch", "--ctr-host",
-            type=str, default='127.0.0.1',
-            help="IP address of SPP controller")
+        "-ch", "--ctr-host",
+        type=str, default='127.0.0.1',
+        help="IP address of SPP controller")
     parser.add_argument(
-            "-cp", "--ctr-port",
-            type=int, default=6666,
-            help="Port of SPP controller")
+        "-cp", "--ctr-port",
+        type=int, default=6666,
+        help="Port of SPP controller")
     return parser.parse_args()
+
 
 def main():
     # Load config
@@ -52,16 +53,15 @@ def main():
     args = parse_args()
 
     cmd_path = '%s/nfv/%s/app/spp_nfv' % (
-            args.sppdir, os.getenv('RTE_TARGET')
-            )
+        args.sppdir, os.getenv('RTE_TARGET'))
+
     if not os.path.isfile(cmd_path):
         cmd_path = '%s/nfv/src/nfv/%s/spp_nfv' % (
-                args.sppdir, os.getenv('RTE_TARGET')
-                )
+            args.sppdir, os.getenv('RTE_TARGET'))
 
     subprocess.call('sudo pwd', shell=True)
-    if args.id != None: # run specified sec
-        ent = y["secondaries"][args.id-1] # sec id is assumed starting from 1
+    if args.id is not None:  # run specified sec
+        ent = y["secondaries"][args.id-1]  # sec id is assumed starting from 1
         logfile = 'log/secondary-%d.log' % ent["id"]
         cmd = 'sudo -E %s \\\n' % cmd_path
         cmd += '  -c %s \\\n' % ent["coremask"]
@@ -73,7 +73,7 @@ def main():
         cmd += '  > %s 2>&1 &' % logfile
         print(cmd)
         subprocess.call(cmd, shell=True)
-    else: # run all of secondaries
+    else:  # run all of secondaries
         for i in range(0, args.num):
             ent = y["secondaries"][i]
             logfile = 'log/secondary-%d.log' % ent["id"]
